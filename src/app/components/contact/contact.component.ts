bcbe4cd739fac1f1648addf2693e8beed07cd6bc
dgbracero <diegobracero@hotmail.com>
2020-12-15 14:27:29 -0500

    Formulario de contacto

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public title: string;
+  public email: string;
+  public telf: string;
 
-  constructor() { }
+  constructor(
+    
+  ) {
+    this.title = "Contacto";
+    this.telf = "0985816922";
+    this.email = "diegobracero@hotmail.com"; 
+  }
 
   ngOnInit(): void {
   }
